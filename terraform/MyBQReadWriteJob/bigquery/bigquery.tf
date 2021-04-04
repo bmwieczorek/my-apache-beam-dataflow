1fa782d3b4b7c080a0de09368d18ee81cba92a37
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2021-04-04 18:53:38 +0200

    case for avro and bigquery nested record and array of records use json instead of csv to pre-load bigquery data

--- terraform/MyBQReadWriteJob/bigquery/bigquery.tf
+++ terraform/MyBQReadWriteJob/bigquery/bigquery.tf
@@ -71,6 +71,7 @@ resource "google_bigquery_job" "my_bigquery_job" {
     source_uris = [
       "gs://${google_storage_bucket_object.my_bucket_object.bucket}/${google_storage_bucket_object.my_bucket_object.name}"
     ]
+    source_format = "NEWLINE_DELIMITED_JSON"
 
     destination_table {
       project_id = var.project
@@ -81,7 +82,7 @@ resource "google_bigquery_job" "my_bigquery_job" {
   }
 
 //  query {
-//    query = "INSERT INTO ${google_bigquery_dataset.my_dataset.dataset_id}.${google_bigquery_table.my_table.table_id} (id,creation_timestamp, expiration_date) values(\"abc\",TIMESTAMP(\"2021-03-03 03:03:03+00\"),DATE '2021-03-03'),(\"def\",CURRENT_TIMESTAMP(),CURRENT_DATE())"
+//    query = "INSERT INTO ${google_bigquery_dataset.my_dataset.dataset_id}.${google_bigquery_table.my_table.table_id} (id,creation_timestamp, expiration_date, numbers) values(\"abc\",TIMESTAMP(\"2021-03-03 03:03:03+00\"),DATE '2021-03-03',[1,2,3]),(\"def\",CURRENT_TIMESTAMP(),CURRENT_DATE(),[5,6,7])"
 //    create_disposition = ""
 //    write_disposition = ""
 //  }
