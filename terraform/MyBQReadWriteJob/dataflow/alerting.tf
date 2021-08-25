6a4416d96155c83fc8e4ab953cffd5ade10268a6
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2021-08-25 13:23:15 +0200

    added cross series reducer to alerts

--- terraform/MyBQReadWriteJob/dataflow/alerting.tf
+++ terraform/MyBQReadWriteJob/dataflow/alerting.tf
@@ -13,38 +13,39 @@ resource "google_monitoring_notification_channel" "email" {
   }
 }
 
-resource "time_sleep" "wait_120_seconds" {
+resource "time_sleep" "wait_100_seconds" {
   depends_on = [google_dataflow_job.my_dataflow_job]
 
-  create_duration = "120s"
+  create_duration = "100s"
 }
 
 
 resource "google_monitoring_alert_policy" "my_alert_job_id" {
-  depends_on = [time_sleep.wait_120_seconds]
+  depends_on = [time_sleep.wait_100_seconds]
 //  depends_on = [google_dataflow_job.my_dataflow_job]
   project = var.project
-  display_name = "${var.job} did not run for last 300s alert policy new"
+  display_name = "${var.job} did not run for last 10m alert policy"
   enabled = true
   combiner = "OR"
 
   notification_channels = [google_monitoring_notification_channel.email.name]
 
   documentation {
-    content = "${var.job} did not run for last 300s documentation - please check new"
+    content = "${var.job} did not run for last 10m documentation - please check"
     mime_type = "text/markdown"
   }
 
   conditions {
-    display_name = "${var.job} did not run for last 300s condition new"
+    display_name = "${var.job} did not run for last 10m condition"
     condition_absent {
       filter        = "metric.type=\"dataflow.googleapis.com/job/element_count\" resource.type=\"dataflow_job\" resource.label.\"job_name\"=monitoring.regex.full_match(\"${var.job}.*\") metric.label.\"pcollection\"=\"BigQueryIO.TypedRead/ReadFiles.out0\""
-      duration      = "300s"
+      duration      = "600s" // 10 min time that a time series must violate the threshold to be considered failing
       trigger {
         percent = 100.0
       }
       aggregations {
-        alignment_period = "300s"
+        alignment_period = "300s" // 5 min aggregation duration, at least 60s
+        cross_series_reducer = "REDUCE_MEAN"
         per_series_aligner = "ALIGN_MEAN"
       }
     }
@@ -53,7 +54,7 @@ resource "google_monitoring_alert_policy" "my_alert_job_id" {
 
 /*
 resource "google_monitoring_alert_policy" "my_alert_job_id" {
-  //depends_on = [time_sleep.wait_120_seconds]
+  //depends_on = [time_sleep.wait_100_seconds]
   project = var.project
   display_name = "${var.job} last run (id=${google_dataflow_job.my_dataflow_job.id}) did not run for last 300s alert policy"
   enabled = true
