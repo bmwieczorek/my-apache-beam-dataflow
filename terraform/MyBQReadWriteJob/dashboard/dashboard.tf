e28b1376ce6c322141a0904845fef5f2a308df19
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2021-10-25 11:48:41 +0200

    removed old approach (non moduled)

--- terraform/MyBQReadWriteJob/dashboard/dashboard.tf
+++
@@ -1,6 +0,0 @@
-resource "google_monitoring_dashboard" "dashboard" {
-  project = var.project
-  dashboard_json = templatefile(var.dashboard_file, {
-    job = var.job
-  })
-}
--- terraform/MyBQReadWriteJob/dataflow/logging.tf
+++
@@ -1,9 +0,0 @@
-resource "google_logging_project_sink" "dataflow-logging-sink" {
-  name        = "${var.job}-logging-sink"
-  destination = "storage.googleapis.com/${var.bucket}"
-  project      = var.project
-  filter      = "resource.type=\"dataflow_step\" resource.labels.job_name=~\"${var.job}.*\" (logName=\"projects/${var.project}/logs/dataflow.googleapis.com%2Fjob-message\" OR logName=\"projects/${var.project}/logs/dataflow.googleapis.com%2Flauncher\") timestamp >= \"${var.dataflow_start_time}\"  severity>=INFO textPayload=\"Worker pool stopped.\""
-  description = "Logging sink for ${var.job}"
-
-  unique_writer_identity = false
-}
