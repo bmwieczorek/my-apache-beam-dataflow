0f70dc4d448b35fafa5793cfcf43aa55461c9c8d
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2021-12-09 10:42:19 +0100

    updated run-terraform.sh

--- terraform/MyPubsubToGCSJob/run-terraform.sh
+++ terraform/MyPubsubToGCSJob/run-terraform.sh
@@ -22,7 +22,7 @@ echo "GCP_OWNER=$GCP_OWNER"
 
 JOB=$(echo "${GCP_OWNER}-$(basename $SCRIPT_DIR)" | tr '[:upper:]' '[:lower:]' )
 echo "JOB=$JOB"
-BUCKET=${JOB}
+BUCKET="${GCP_PROJECT}-${JOB}"
 echo "BUCKET=$BUCKET"
 TOPIC=${GCP_OWNER}-topic
 echo "TOPIC=$TOPIC"
@@ -38,26 +38,22 @@ gcloud monitoring dashboards list --filter="displayName='${JOB} job name'" --for
 gcloud monitoring dashboards list --filter="displayName='${JOB} redesigned job id'" --format 'value(NAME)' | xargs gcloud monitoring dashboards delete --quiet
 gcloud monitoring dashboards list --filter="displayName='${JOB} redesigned job name'" --format 'value(NAME)' | xargs gcloud monitoring dashboards delete --quiet
 
-#gcloud monitoring dashboards list --filter="displayName='airshopping-data-ingestion redesigned job name'" --format 'value(NAME)' | xargs gcloud monitoring dashboards delete --quiet
-#gcloud monitoring dashboards list --filter="displayName='robotic-shopping redesigned job name'" --format 'value(NAME)' | xargs gcloud monitoring dashboards delete --quiet
-#gcloud monitoring dashboards list --filter="displayName='robotic-shopping redesigned job id 2021-05-27_05_53_24-8109596674050890303'" --format 'value(NAME)' | xargs gcloud monitoring dashboards delete --quiet
-#gcloud monitoring dashboards list --filter="displayName='airshopping-data-ingestion redesigned job id 2021-05-26_09_34_30-17149002215298212728'" --format 'value(NAME)' | xargs gcloud monitoring dashboards delete --quiet
-
 gcloud dataflow jobs list --filter "NAME:${JOB} AND STATE=Running" --format 'value(JOB_ID)' --region "$GCP_REGION" | xargs gcloud dataflow jobs cancel --region "${GCP_REGION}"
 max_retry=10; counter=1; sleep_secs=5; until [ -z "$(gcloud dataflow jobs list --filter "NAME:${JOB} AND (STATE=Cancelling OR STATE=Running)" --format 'value(JOB_ID)' --region ${GCP_REGION})" ] ; do sleep $sleep_secs; [[ counter -eq $max_retry ]] && echo "Failed" && break; echo "waiting $sleep_secs secs for job to stop: attempt $counter" ; ((counter++)); done
+
 gsutil rm -r gs://${BUCKET}
+gsutil -q stat "gs://${GCP_OWNER}-terraform/**" || gsutil mb "gs://${GCP_OWNER}-terraform"
 
 export TF_VAR_project="$GCP_PROJECT"
 export TF_VAR_region="$GCP_REGION"
 export TF_VAR_owner="$GCP_OWNER"
 export TF_VAR_bucket="$BUCKET"
 
-gsutil -q stat "gs://${GCP_OWNER}-terraform/**" || gsutil mb "gs://${GCP_OWNER}-terraform"
 
 >$SCRIPT_DIR/backend.tf cat <<-EOF
 terraform {
   backend "gcs" {
-    bucket = "$GCP_OWNER-terraform"
+    bucket = "$GCP_PROJECT-$GCP_OWNER-terraform"
     prefix = "$JOB/tfstate"
   }
   required_providers {
@@ -68,7 +64,6 @@ terraform {
   required_version = "~> 0.12.29"
 }
 EOF
-
 
 >$SCRIPT_DIR/dev.tfvars cat <<-EOF
 topic="${TOPIC}"
@@ -83,5 +78,4 @@ EOF
 
 run_terraform
 
-#java -jar ~/Downloads/avro-tools-1.8.1.jar tojson
 
