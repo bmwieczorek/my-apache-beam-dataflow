7127510e998598619e3f9252faf52e60bd1bd32f
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-05-01 07:02:18 +0200

    added storage location

--- terraform/MyBQReadWriteJob/storage/storage.tf
+++ terraform/MyBQReadWriteJob/storage/storage.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "bucket" {
   project	    = var.project
   name          = var.bucket
+  location      = "US"
   force_destroy = true
   labels = {
     owner   = var.owner
