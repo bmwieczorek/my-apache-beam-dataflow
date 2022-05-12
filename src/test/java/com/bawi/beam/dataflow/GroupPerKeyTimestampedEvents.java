db70048988693e051dcc13e2549bd3698aa0d1ce
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-05-12 09:21:14 +0200

    refactored to group per key

---
+++ src/test/java/com/bawi/beam/dataflow/GroupPerKeyTimestampedEvents.java
@@ -0,0 +1,87 @@
+package com.bawi.beam.dataflow;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.TimestampCombiner;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TimestampedValue;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.junit.Test;
+
+public class GroupPerKeyTimestampedEvents {
+    
+
+    private static class MyStringFormatFn extends DoFn<KV<String, ? extends Number>, String> {
+        @ProcessElement
+        public void process(@Element KV<String, ? extends Number> element, @Timestamp Instant timestamp, BoundedWindow window, OutputReceiver<String> outputReceiver) {
+            outputReceiver.output(String.format("e=%s,ts=%s,wMaxTs=%s,wCl=%s,w=%s", element, timestamp, window.maxTimestamp(), window.getClass().getSimpleName(), window));
+        }
+    }
+
+    Create.TimestampedValues<KV<String, Integer>> timestampedEvents = Create.timestamped(
+        // W1 [0-5) s
+        TimestampedValue.of(KV.of("a", 1), Instant.parse("2021-12-16T00:00:00Z")),
+        TimestampedValue.of(KV.of("a", 2), Instant.parse("2021-12-16T00:00:01Z")),
+
+        // W2 [5,10) s
+        TimestampedValue.of(KV.of("b", 10), Instant.parse("2021-12-16T00:00:05Z")),
+        TimestampedValue.of(KV.of("c", 20), Instant.parse("2021-12-16T00:00:06Z")),
+        TimestampedValue.of(KV.of("a", 30), Instant.parse("2021-12-16T00:00:07Z"))
+    );
+
+    @Test
+    public void shouldCountPerKey() {
+        // given
+        Pipeline pipeline = Pipeline.create();
+        PCollection<String> pCollection = pipeline.apply(timestampedEvents)
+
+        // when
+        // group per 5 secs window and count how many elements per key in each window
+        .apply(Window.<KV<String, Integer>>into(FixedWindows.of(Duration.standardSeconds(5))).withTimestampCombiner(TimestampCombiner.LATEST)) // timestamp combiner impacts what will be the grouped event timestamp in relation to ts of grouped events
+        .apply(Count.perKey())
+        .apply(ParDo.of(new MyStringFormatFn()));
+
+        // then
+        PAssert.that(pCollection).containsInAnyOrder(
+                // W1 [0-5) s
+                "e=KV{a, 2},ts=2021-12-16T00:00:01.000Z,wMaxTs=2021-12-16T00:00:04.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:00.000Z..2021-12-16T00:00:05.000Z)", // 2 x "a" grouped as belonging to same window 
+
+                // W2 [5,10) s
+                "e=KV{b, 1},ts=2021-12-16T00:00:05.000Z,wMaxTs=2021-12-16T00:00:09.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:05.000Z..2021-12-16T00:00:10.000Z)",
+                "e=KV{c, 1},ts=2021-12-16T00:00:06.000Z,wMaxTs=2021-12-16T00:00:09.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:05.000Z..2021-12-16T00:00:10.000Z)",
+                "e=KV{a, 1},ts=2021-12-16T00:00:07.000Z,wMaxTs=2021-12-16T00:00:09.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:05.000Z..2021-12-16T00:00:10.000Z)"  // latest "a" belongs to next window
+        );
+        pipeline.run().waitUntilFinish();
+    }
+
+    @Test
+    public void shouldSumPerKey() {
+        // given
+        Pipeline pipeline = Pipeline.create();
+        PCollection<String> pCollection = pipeline.apply(timestampedEvents)
+
+                // when
+                // group per 5 secs window and sum elements value per key in each window
+                .apply(Window.<KV<String, Integer>>into(FixedWindows.of(Duration.standardSeconds(5))).withTimestampCombiner(TimestampCombiner.LATEST)) // timestamp combiner impacts what will be the grouped event timestamp in relation to ts of grouped events
+                .apply(Sum.integersPerKey())
+                .apply(ParDo.of(new MyStringFormatFn()));
+
+        // then
+        PAssert.that(pCollection).containsInAnyOrder(
+                // W1 [0-5) s
+                "e=KV{a, 3},ts=2021-12-16T00:00:01.000Z,wMaxTs=2021-12-16T00:00:04.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:00.000Z..2021-12-16T00:00:05.000Z)", // 2 x "a" grouped as belonging to same window
+
+                // W2 [5,10) s
+                "e=KV{b, 10},ts=2021-12-16T00:00:05.000Z,wMaxTs=2021-12-16T00:00:09.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:05.000Z..2021-12-16T00:00:10.000Z)",
+                "e=KV{c, 20},ts=2021-12-16T00:00:06.000Z,wMaxTs=2021-12-16T00:00:09.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:05.000Z..2021-12-16T00:00:10.000Z)",
+                "e=KV{a, 30},ts=2021-12-16T00:00:07.000Z,wMaxTs=2021-12-16T00:00:09.999Z,wCl=IntervalWindow,w=[2021-12-16T00:00:05.000Z..2021-12-16T00:00:10.000Z)"  // latest "a" belongs to next window
+        );
+        pipeline.run().waitUntilFinish();
+    }
+}
