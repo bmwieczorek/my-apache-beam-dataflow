a8474cb3fe7a19b3c3ed02e64034e4f1addebdae
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-07-19 18:10:21 +0200

    added GPG PGP encryption decryption test

---
+++ src/test/java/com/bawi/pgp/GPGEncryptionDecryptionTest.java
@@ -0,0 +1,98 @@
+package com.bawi.pgp;
+
+import name.neuhalfen.projects.crypto.bouncycastle.openpgp.BouncyGPG;
+import name.neuhalfen.projects.crypto.bouncycastle.openpgp.keys.callbacks.KeyringConfigCallbacks;
+import name.neuhalfen.projects.crypto.bouncycastle.openpgp.keys.keyrings.InMemoryKeyring;
+import name.neuhalfen.projects.crypto.bouncycastle.openpgp.keys.keyrings.KeyringConfigs;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.util.io.Streams;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.security.SignatureException;
+
+public class GPGEncryptionDecryptionTest {
+    @Test
+    public void test() throws IOException, PGPException, NoSuchProviderException, SignatureException, NoSuchAlgorithmException {
+        // given
+        String publicKey="-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+                // ...
+                "-----END PGP PUBLIC KEY BLOCK-----";
+
+        String privateKey = "-----BEGIN PGP PRIVATE KEY BLOCK-----\n" +
+                // ...
+                "-----END PGP PRIVATE KEY BLOCK-----";
+
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+//        InputStream pubStream = new ByteArrayInputStream(publicKey.getBytes());
+//        InputStream secStream = new ByteArrayInputStream(privateKey.getBytes());
+//        KeyringConfig keyringConfig =
+//                KeyringConfigs.withKeyRingsFromStreams(pubStream, secStream, KeyringConfigCallbacks.withPassword("bartek"));
+
+        InMemoryKeyring keyringConfig =
+                KeyringConfigs.forGpgExportedKeys(KeyringConfigCallbacks.withPassword("bartek"));
+
+        keyringConfig.addPublicKey(publicKey.getBytes());
+        keyringConfig.addSecretKey(privateKey.getBytes());
+
+        String plainText = "hello world";
+        byte[] encryptedBytes;
+        byte[] decryptedBytes;
+        try (
+                ByteArrayInputStream plainTextInputStream = new ByteArrayInputStream(plainText.getBytes());
+                ByteArrayOutputStream encryptedOutputStream = new ByteArrayOutputStream()
+        ) {
+            encrypt(keyringConfig, plainTextInputStream, encryptedOutputStream, "bartek@example.com");
+            encryptedBytes = encryptedOutputStream.toByteArray();
+        }
+
+        try (
+                ByteArrayInputStream encryptedInputStream = new ByteArrayInputStream(encryptedBytes);
+                ByteArrayOutputStream decryptedOutputStream = new ByteArrayOutputStream()
+        ) {
+            decrypt(keyringConfig, encryptedInputStream, decryptedOutputStream);
+            decryptedBytes = decryptedOutputStream.toByteArray();
+        }
+
+        Assert.assertEquals(plainText, new String(decryptedBytes));
+
+//        encrypt(keyringConfig, new FileInputStream("a_plain.txt"), new FileOutputStream("a.pgp"), "bartek@example.com");
+//        decrypt(keyringConfig, new FileInputStream("a.gpg"), new FileOutputStream("a_decrypted.txt"));
+    }
+
+    private void encrypt(InMemoryKeyring keyringConfig, InputStream inputStream, OutputStream outputStream, String recipient) throws IOException, PGPException, NoSuchAlgorithmException, SignatureException, NoSuchProviderException {
+        try (
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+
+                OutputStream encryptedStream = BouncyGPG
+                    .encryptToStream()
+                    .withConfig(keyringConfig)
+                    .withStrongAlgorithms()
+                    .toRecipient(recipient)
+                    .andDoNotSign()
+                    .binaryOutput()
+                    .andWriteTo(bufferedOutputStream)
+        ) {
+            Streams.pipeAll(inputStream, encryptedStream);
+        }
+    }
+
+    private void decrypt(InMemoryKeyring keyringConfig, InputStream encryptedInputStream, OutputStream outputStream)
+            throws IOException, NoSuchProviderException {
+        try (
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+
+                InputStream decryptedStream = BouncyGPG
+                        .decryptAndVerifyStream()
+                        .withConfig(keyringConfig)
+                        .andIgnoreSignatures()
+                        .fromEncryptedInputStream(encryptedInputStream)
+        ) {
+            Streams.pipeAll(decryptedStream, bufferedOutputStream);
+        }
+    }
+}
