cb17b65ad6c09da7693825b35e5b9d074c0c2a16
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-08-19 10:12:56 +0200

    added stub comments

--- src/main/python/stub/my_main.py
+++ src/main/python/stub/my_main.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
     # stringify in example.pyi (stub) defines input and return types but no implementation
     # IDE allows type validation: Expected type 'str', got 'int' instead
     # stringify(1)  # Expected type 'str', got 'int' instead
+
+    # go to imp navigates to example.py, quick doc shows types defined in example.pyi stub
     s = stringify('a')
     print(f"s={s}")
