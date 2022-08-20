d82b3d967101b529739967b5e3c0fabac7100d43
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-08-20 10:34:14 +0200

    added more examples to python if else statement

--- src/main/python/my_if_else.py
+++ src/main/python/my_if_else.py
@@ -4,7 +4,18 @@ def print_conditionally(_flag):
 
 
 if __name__ == '__main__':
+    print_conditionally(0)  # 0
     print_conditionally(None)  # 0
     print_conditionally(False)  # 0
+    print_conditionally("")  # 0
+    print_conditionally([])  # 0
+    print_conditionally(())  # 0
+    print_conditionally(list())  # 0
+    print_conditionally(dict())  # 0
+    print('-----------------------')
+    print_conditionally(1)  # 1
     print_conditionally("a")  # 1
     print_conditionally(True)  # 1
+    print_conditionally(['a'])  # 1
+    print_conditionally(('a', 'b'))  # 1
+    print_conditionally({'a': 1})  # 1
