a2b3a28988aa462856479fbe89b1ae49036e8f06
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-09-05 19:48:13 +0200

    updated DataflowUtils to check for null

--- src/main/java/com/bawi/beam/dataflow/DataflowUtils.java
+++ src/main/java/com/bawi/beam/dataflow/DataflowUtils.java
@@ -1,17 +1,42 @@
 package com.bawi.beam.dataflow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class DataflowUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataflowUtils.class);
+
     public static String[] updateDataflowArgs(String[] args, String... additionalArgs) {
-        return Stream.of(
-                   args,
-                   System.getenv("GCP_JAVA_DATAFLOW_RUN_OPTS").split(" +"),
-                   new String[] {
-                       "--runner=DataflowRunner",
-                       "--stagingLocation=gs://" + System.getenv("GCP_PROJECT") + "-" + System.getenv("GCP_OWNER") + "/staging"
-                   },
-                   additionalArgs
-               ).flatMap(Stream::of).toArray(String[]::new);
+        Set<String> merged = new HashSet<>();
+        merged.add("--runner=DataflowRunner");
+        if (System.getenv("GCP_JAVA_DATAFLOW_RUN_OPTS") != null) {
+            merged.addAll(Arrays.asList(System.getenv("GCP_JAVA_DATAFLOW_RUN_OPTS").split(" +")));
+        }
+        if (System.getenv("GCP_PROJECT") != null) {
+            if (!argsContain(args, "projectId")) {
+                merged.add("--projectId=" + System.getenv("GCP_PROJECT"));
+            }
+            if (!argsContain(args, "stagingLocation") && System.getenv("GCP_OWNER") != null) {
+                merged.add("--stagingLocation=gs://" + System.getenv("GCP_PROJECT") + "-" + System.getenv("GCP_OWNER") + "/staging");
+            }
+            if (!argsContain(args, "tempLocation") && System.getenv("GCP_OWNER") != null) {
+                merged.add("--tempLocation=gs://" + System.getenv("GCP_PROJECT") + "-" + System.getenv("GCP_OWNER") + "/temp");
+            }
+        }
+
+        merged.addAll(Arrays.asList(args));
+        merged.addAll(Arrays.asList(additionalArgs));
+        String[] strings = merged.toArray(String[]::new);
+        LOGGER.info("Merged args={}", Arrays.asList(merged));
+        return strings;
+    }
+
+    private static boolean argsContain(String[] args, String attribute) {
+        return Stream.of(args).anyMatch(s -> s.startsWith("--" + attribute + "="));
     }
 }
