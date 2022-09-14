adaeec82b7d54bac626646e865c3bf7610cf127f
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-09-14 20:42:49 +0200

    updated MySimpleLoggingJob to include bundle size

--- src/main/java/com/bawi/beam/dataflow/MySimpleLoggingJob.java
+++ src/main/java/com/bawi/beam/dataflow/MySimpleLoggingJob.java
@@ -2,12 +2,22 @@ package com.bawi.beam.dataflow;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.metrics.Distribution;
+import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.bawi.beam.dataflow.MySimpleLoggingJob.MyDoFn.THREAD_IDS;
 
 public class MySimpleLoggingJob {
     private static final Logger LOGGER = LoggerFactory.getLogger(MySimpleLoggingJob.class);
@@ -34,17 +44,48 @@ mvn compile -DskipTests -Pdataflow-runner exec:java \
 
 */
 
-    public static void main(String[] args) {
-        Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).create());
-        pipeline.apply(Create.of("1", "2", "3"))
-                .apply(MapElements.into(TypeDescriptors.strings()).via(s -> {
-                    LOGGER.info("[LOGGER] Processing: {}", s);
-                    System.out.println("[Console] Processing: " + s);
-                    int i = Integer.parseInt(s);
-                    return "i=" + i;
-                }));
-        PipelineResult result = pipeline.run();
+    static class MyDoFn extends DoFn<Integer,Integer> {
+        static Set<Long> THREAD_IDS = new HashSet<>();
+        private final Distribution bundleSizeDistribution = Metrics.distribution(MyDoFn.class, "bundleSizeDist");
 
+        private Integer bundleSize;
+
+        @StartBundle
+        public void startBundle() {
+            bundleSize = 0;
+        }
+
+        @ProcessElement
+        public void process(@Element Integer i, OutputReceiver<Integer> receiver) {
+            bundleSize++;
+            long id = Thread.currentThread().getId();
+            THREAD_IDS.add(id);
+            LOGGER.info("[tid={}] Processing: {}", id, i);
+            receiver.output(i);
+        }
+
+        @FinishBundle
+        public void finishBundle() {
+            LOGGER.info("[tid={}] Bundle size is {}", Thread.currentThread().getId(), bundleSize);
+            bundleSizeDistribution.update(bundleSize);
+            bundleSize=0;
+
+        }
+    }
+    public static void main(String[] args) {
+//        args = DataflowUtils.updateDataflowArgs(args);
+        Pipeline pipeline = Pipeline.create(PipelineOptionsFactory.fromArgs(args).create());
+        List<Integer> ints = IntStream.rangeClosed(1, 1000).boxed().collect(Collectors.toList());
+        pipeline.apply(Create.of(ints))
+                .apply(ParDo.of(new MyDoFn()))
+//                .apply(MapElements.into(TypeDescriptors.integers()).via(i -> {
+//                    LOGGER.info("[tid={}] Processing: {}", Thread.currentThread().getId(), i);
+//                    return i;
+//                }))
+        ;
+        PipelineResult result = pipeline.run();
+        result.metrics().allMetrics().getDistributions().forEach(m -> LOGGER.info("{}: {}", m.getName(), m.getCommitted()));
+        LOGGER.info("{} thread ids: {}", THREAD_IDS.size(), THREAD_IDS);  // Math.max(Runtime.getRuntime().availableProcessors(), 3)
         // usually waitUntilFinish while pipeline development, remove when generating dataflow classic template
         //result.waitUntilFinish();
     }
