368e046aad3097dec1ed61fe25714714c2e8eabe
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-10-09 11:58:30 +0200

    updated updated my beam pipeline

--- src/main/python/beam/my_pipeline.py
+++ src/main/python/beam/my_pipeline.py
@@ -1,15 +1,23 @@
 import argparse
-import logging
+
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
 
-# based on https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/wordcount.py
+# based on
+# https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/wordcount.py
+
 # pip install pytest apache-beam['gcp']
+
+# direct runner
 # python3 src/main/python/beam/my_pipeline.py
-# python3 src/main/python/beam/my_pipeline.py --project ${GCP_PROJECT} --region ${GCP_REGION} --runner DataflowRunner \
-# --temp_location gs://${GCP_PROJECT}-${GCP_OWNER}/python-tmp/ --service_account_email=${GCP_SERVICE_ACCOUNT} \
-# --no_use_public_ips
+
+# dataflow runner
+# python3 src/main/python/beam/my_pipeline.py --project ${GCP_PROJECT} \
+# --region ${GCP_REGION} --runner DataflowRunner \
+# --temp_location gs://${GCP_PROJECT}-${GCP_OWNER}/python-tmp/ \
+# --service_account_email=${GCP_SERVICE_ACCOUNT} --no_use_public_ips
+
 
 def run(argv=None):
     parser = argparse.ArgumentParser()
@@ -18,12 +26,15 @@ def run(argv=None):
 
     # The pipeline will be run on exiting the with block.
     with beam.Pipeline(options=pipeline_options) as p:
-        # IDE unnecessary warning as PTransform implements __ror__
         # noinspection PyUnresolvedReferences
         words = p | 'Create' >> beam.Create(["hi", "there", "bob"])
+        # noinspection PyUnresolvedReferences
         words | 'Log' >> beam.Map(lambda word: logging.info(word))
 
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
+    import logging
+    import sys
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format='%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s')
     run()
