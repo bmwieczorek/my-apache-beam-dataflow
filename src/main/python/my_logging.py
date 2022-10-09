54d7948e255e7e612ffdb030f85ffcde3bb5a8c3
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-10-09 12:04:26 +0200

    updated my_logging.py

---
+++ src/main/python/my_logging.py
@@ -0,0 +1,12 @@
+
+if __name__ == '__main__':
+    import logging
+    import sys
+    # logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    #                     format='%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s')
+    file_handler = logging.FileHandler(filename='my_logging.py.log')
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    handlers = [file_handler, stdout_handler]
+    logging.basicConfig(level=logging.DEBUG, handlers=handlers,
+                        format='%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s')
+    logging.info("hello")
