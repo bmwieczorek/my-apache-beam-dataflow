9cea5df8650e8de053f2e7aa86f8c4fd12d08cec
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-10-09 11:12:36 +0200

    updated multi output test

--- src/test/java/com/bawi/beam/dataflow/PipelineMultiOutputTest.java
+++ src/test/java/com/bawi/beam/dataflow/PipelineMultiOutputTest.java
@@ -1,49 +1,30 @@
 package com.bawi.beam.dataflow;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.coders.VarIntCoder;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
-import org.apache.beam.sdk.values.TupleTag;
-import org.apache.beam.sdk.values.TupleTagList;
 import org.junit.Test;
 
+import static com.bawi.beam.dataflow.MyMultiOutputJob.*;
+
 public class PipelineMultiOutputTest {
-
-    static TupleTag<Integer> positiveTag = new TupleTag<>("positive");
-    static TupleTag<Integer> zeroTag = new TupleTag<>("zero");
-    static TupleTag<Integer> negativeTag = new TupleTag<>("negative");
-
-    static class DispatchingFn extends DoFn<Integer, Integer> {
-        @ProcessElement
-        public void process(@Element Integer element, MultiOutputReceiver multiOutputReceiver) {
-            if (element > 0) multiOutputReceiver.get(positiveTag).output(element);
-            if (element == 0) multiOutputReceiver.get(zeroTag).output(element);
-            if (element < 0) multiOutputReceiver.get(negativeTag).output(element);
-        }
-    }
 
     @Test
     public void testMultiOutput() {
         Pipeline pipeline = Pipeline.create();
 
         PCollectionTuple multipleCollections = pipeline
-                .apply(Create.of(1, -1, 2, 0, -2))
-                .apply(ParDo.of(new DispatchingFn())
-                            .withOutputTags(positiveTag, TupleTagList.of(zeroTag).and(negativeTag))
-                );
+                .apply(Create.of(1, 2, 3))
+                .apply(new MyMultiOutputJob.MyMultiPTransform());
 
-        PCollection<Integer> positive = multipleCollections.get(positiveTag).setCoder(VarIntCoder.of());
-        PCollection<Integer> zero = multipleCollections.get(zeroTag).setCoder(VarIntCoder.of());
-        PCollection<Integer> negative = multipleCollections.get(negativeTag).setCoder(VarIntCoder.of());
+        PCollection<String> even = multipleCollections.get(evenTag).setCoder(StringUtf8Coder.of());
+        PCollection<String> odd = multipleCollections.get(oddTag).setCoder(StringUtf8Coder.of());
 
-        PAssert.that(positive).containsInAnyOrder(1, 2);
-        PAssert.that(zero).containsInAnyOrder(0);
-        PAssert.that(negative).containsInAnyOrder(-1, -2);
+        PAssert.that(even).containsInAnyOrder("2");
+        PAssert.that(odd).containsInAnyOrder("1", "3");
 
         pipeline.run().waitUntilFinish();
     }
