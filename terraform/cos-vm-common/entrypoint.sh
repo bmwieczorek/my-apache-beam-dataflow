3f49b7cfd4f48545489b3080183afeea4404458a
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2022-10-24 11:18:42 +0200

    added docker testing and deployment to gcp

---
+++ terraform/cos-vm-common/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "$0: HOME=$HOME"
+echo "$0: USER=$USER"
+echo "$0: MYARG=$MYARG"
+echo "$0: MYENV=$MYENV"
+echo "$0: MY_DOCKER_RUN_COMMAND_LINE_ENV=$MY_DOCKER_RUN_COMMAND_LINE_ENV"
+
+#source ~/.bash_profile
+echo "$0: MYBASHVAR=$MYBASHVAR"
+
