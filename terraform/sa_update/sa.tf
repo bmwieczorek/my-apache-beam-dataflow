dfdfcf747a4fa293e99c290882a487445a8a6199
Bartosz Wieczorek <bmwieczorek@gmail.com>
2022-12-19 15:51:32 +0100

    added output

--- terraform/sa_update/sa.tf
+++ terraform/sa_update/sa.tf
@@ -53,3 +53,25 @@ resource "null_resource" "print-sa-roles-2" {
     EOF
   }
 }
+
+#
+#terraform apply -auto-approve
+#google_service_account.sa: Creating...
+#google_service_account.sa: Creation complete after 1s
+#google_project_iam_member.add_bigquery_role_to_sa_for_project: Creating...
+#google_project_iam_member.add_bigquery_role_to_sa_for_project: Creation complete after 9s [id=.../roles/bigquery.user/serviceAccount:...]
+#google_project_iam_member.add_bigtable_role_to_sa_for_project: Creating...
+#null_resource.print-sa-roles-1: Creating...
+#null_resource.print-sa-roles-1: Provisioning with 'local-exec'...
+#null_resource.print-sa-roles-1 (local-exec): Executing: ["/bin/sh" "-c" "gcloud projects get-iam-policy ...]
+#null_resource.print-sa-roles-1 (local-exec): ROLE
+#null_resource.print-sa-roles-1 (local-exec): roles/bigquery.user
+#null_resource.print-sa-roles-1: Creation complete after 2s [id=8002834195468491454]
+#google_project_iam_member.add_bigtable_role_to_sa_for_project: Creation complete after 9s [id=.../roles/bigtable.user/serviceAccount:...]
+#null_resource.print-sa-roles-2: Creating...
+#null_resource.print-sa-roles-2: Provisioning with 'local-exec'...
+#null_resource.print-sa-roles-2 (local-exec): Executing: ["/bin/sh" "-c" "gcloud projects get-iam-policy ...]
+#null_resource.print-sa-roles-2 (local-exec): ROLE
+#null_resource.print-sa-roles-2 (local-exec): roles/bigquery.user
+#null_resource.print-sa-roles-2 (local-exec): roles/bigtable.user
+#null_resource.print-sa-roles-2: Creation complete after 1s [id=8694381192174481348]
