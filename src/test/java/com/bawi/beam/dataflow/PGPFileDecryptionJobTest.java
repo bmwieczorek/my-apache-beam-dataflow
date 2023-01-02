921e989808f88e35483c7d38fb9c1cfcb63bad48
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-01-02 10:18:50 +0100

    added RetrieveGCPSecretTest to detect sandbox env for decompression

--- src/test/java/com/bawi/beam/dataflow/PGPFileDecryptionJobTest.java
+++ src/test/java/com/bawi/beam/dataflow/PGPFileDecryptionJobTest.java
@@ -3,10 +3,12 @@ package com.bawi.beam.dataflow;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.values.PCollection;
+import org.junit.Assume;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.Serializable;
+import java.nio.file.Paths;
 
 public class PGPFileDecryptionJobTest implements Serializable {
 
@@ -15,7 +17,9 @@ public class PGPFileDecryptionJobTest implements Serializable {
 
     @Test
     public void test() {
-        PCollection<String> pCollection = pipeline.apply(new PGPDecryptJob.PGPDecryptPTransform("hello-pp.txt.gpg", System.getenv("GCP_PROJECT"),
+        Assume.assumeTrue("Requires sandbox gcp project", System.getenv("GCP_PROJECT").contains("-01-"));
+        String filePattern = Paths.get("src", "test", "resources", "hello-pp.txt.gpg").toString();
+        PCollection<String> pCollection = pipeline.apply(new PGPDecryptJob.PGPDecryptPTransform(filePattern, System.getenv("GCP_PROJECT"),
                 "bartosz-private-key", "bartosz-private-key-passphrase"));
 
         // assert
