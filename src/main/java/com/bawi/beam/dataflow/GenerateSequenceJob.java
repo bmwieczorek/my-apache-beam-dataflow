891173b4ca8125906975aafd597f57303febb0b4
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-01-02 12:45:25 +0100

    added GenerateSequenceJob

--- src/main/java/com/bawi/beam/dataflow/GenerateSequenceJob.java
+++ src/main/java/com/bawi/beam/dataflow/GenerateSequenceJob.java
@@ -2,25 +2,24 @@ package com.bawi.beam.dataflow;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
-import org.apache.beam.sdk.io.GenerateSequence;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.values.PCollection;
 import org.joda.time.Duration;
 
-public class GenerateSequenceTest {
+public class GenerateSequenceJob {
     public static void main(String[] args) {
         args = DataflowUtils.updateDataflowArgs(args);
         PipelineOptions options = PipelineOptionsFactory.fromArgs(args).as(PipelineOptions.class);
 
         Pipeline pipeline = Pipeline.create(options);
-        PCollection<Long> apply = pipeline.apply(GenerateSequence
+        PCollection<Long> apply = pipeline.apply(org.apache.beam.sdk.io.GenerateSequence
                 .from(1)
                 //.to(60) // bounded
                 .withRate(20, Duration.standardSeconds(1L)));
         apply
                 .apply(MyConsoleIO.write());
         PipelineResult run = pipeline.run();
-//        run.waitUntilFinish();
+        run.waitUntilFinish();
     }
 }
