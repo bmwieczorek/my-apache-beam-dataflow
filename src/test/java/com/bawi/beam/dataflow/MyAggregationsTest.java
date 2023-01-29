e432cc5195be870b1d70d9821d06cb9b09c544b8
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-01-29 14:28:39 +0100

    Added more tests for aggregations

--- src/test/java/com/bawi/beam/dataflow/MyAggregationsTest.java
+++ src/test/java/com/bawi/beam/dataflow/MyAggregationsTest.java
@@ -1,21 +1,31 @@
 package com.bawi.beam.dataflow;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.IterableCoder;
+import org.apache.beam.sdk.coders.KvCoder;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.coders.VarIntCoder;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.PaneInfo;
+import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TimestampedValue;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.joda.time.Duration;
 import org.joda.time.Instant;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+
+import static com.bawi.beam.dataflow.LogUtils.windowToString;
 
 public class MyAggregationsTest {
     private static Pipeline getPipeline(Runner runner) {
@@ -111,9 +121,47 @@ public class MyAggregationsTest {
     public void testCountPerKey() {
         Pipeline pipeline = Pipeline.create();
         PCollection<KV<String,Long>> result = pipeline.apply(Create.of(KV.of("a", 1), KV.of("b", 1), KV.of("a", 2)))
+                // same as Combine.perKey(new CountFn<V>());
                 .apply(Count.perKey())
                 .apply(new Log<>());
         PAssert.that(result).containsInAnyOrder(List.of(KV.of("a", 2L), KV.of("b", 1L)));
+        pipeline.run().waitUntilFinish();
+    }
+
+    @Test
+    public void testCombinePerKeyWithSum() {
+        Pipeline pipeline = Pipeline.create();
+        PCollection<KV<String,Integer>> result =
+                pipeline.apply(Create.of(KV.of("a", 1), KV.of("a", 2), KV.of("b", 10), KV.of("b", 20)))
+                        // same as Sum.integersPerKey()
+                        .apply(Combine.perKey(Sum.ofIntegers()))
+                        .apply(new Log<>());
+        PAssert.that(result).containsInAnyOrder(List.of(KV.of("a", 3), KV.of("b", 30)));
+        pipeline.run().waitUntilFinish();
+    }
+
+    @Test
+    public void testGroupedValues() {
+        Pipeline pipeline = Pipeline.create();
+        PCollection<KV<String,Integer>> result =
+        pipeline.apply(Create.<KV<String, Iterable<Integer>>>of(KV.of("a", List.of(1, 2)), KV.of("b", List.of(10, 20)))
+                                .withCoder(KvCoder.of(StringUtf8Coder.of(), IterableCoder.of(VarIntCoder.of()))))
+                .apply(Combine.groupedValues(Sum.ofIntegers()))
+                .apply(new Log<>());
+        PAssert.that(result).containsInAnyOrder(List.of(KV.of("a", 3), KV.of("b", 30)));
+        pipeline.run().waitUntilFinish();
+    }
+
+
+    @Test
+    public void testGroupPerKeyAndGroupedValues() {
+        Pipeline pipeline = Pipeline.create();
+        PCollection<KV<String,Integer>> result =
+                pipeline.apply(Create.of(KV.of("a", 1), KV.of("a", 2), KV.of("b", 10), KV.of("b", 20)))
+                        .apply(GroupByKey.create())
+                        .apply(Combine.groupedValues(Sum.ofIntegers()))
+                        .apply(new Log<>());
+        PAssert.that(result).containsInAnyOrder(List.of(KV.of("a", 3), KV.of("b", 30)));
         pipeline.run().waitUntilFinish();
     }
 
@@ -131,9 +179,48 @@ public class MyAggregationsTest {
     @Test
     public void testMyCombineGlobally() {
         Pipeline pipeline = Pipeline.create();
-        pipeline.apply(Create.of(KV.of("a", 1), KV.of("b", 1), KV.of("a", 2)))
-                .apply(Combine.globally(new MyToDelimitedString<>()))
+        PCollection<Integer> result = pipeline.apply(Create.of(1, 2, 3, 4))
+                .apply(Combine.globally(Sum.ofIntegers()))
+                .apply(new Log<>());// 10
+        PAssert.that(result).containsInAnyOrder(10);
+        pipeline.run().waitUntilFinish();
+    }
+
+
+    @Test
+    public void testMyCombineWindowGlobally() {
+        Pipeline pipeline = Pipeline.create();
+        pipeline.apply(Create.timestamped(
+                        // W1 [0-5) s
+                        TimestampedValue.of(1, Instant.parse("2021-12-16T00:00:00Z")),
+                        TimestampedValue.of(2, Instant.parse("2021-12-16T00:00:01Z")),
+
+                        // W2 [5,10) s
+                        TimestampedValue.of(10, Instant.parse("2021-12-16T00:00:05Z")),
+                        TimestampedValue.of(20, Instant.parse("2021-12-16T00:00:06Z"))
+                ))
+                .apply(Combine.globally(Sum.ofIntegers())) // uses by default global window so sum of all elements despite their timestamps
+                .apply(new Log<>()); //
+        pipeline.run().waitUntilFinish();
+    }
+
+    @Test
+    public void testMyCombineWindow() {
+        Pipeline pipeline = Pipeline.create();
+        PCollection<Integer> result = pipeline.apply(Create.timestamped(
+                        // W1 [0-5) s
+                        TimestampedValue.of(1, Instant.parse("2021-12-16T00:00:00Z")),
+                        TimestampedValue.of(2, Instant.parse("2021-12-16T00:00:01Z")),
+
+                        // W2 [5,10) s
+                        TimestampedValue.of(10, Instant.parse("2021-12-16T00:00:05Z")),
+                        TimestampedValue.of(20, Instant.parse("2021-12-16T00:00:06Z"))
+                ))
+                .apply(Window.into(FixedWindows.of(Duration.standardSeconds(5))))
+//                .apply(Window.into(new GlobalWindows())) // re-window to global or bigger window would aggregate all elements in all windows
+                .apply(Sum.integersGlobally().withoutDefaults()) // sum elements within each window separately
                 .apply(new Log<>());
+        PAssert.that(result).containsInAnyOrder(3, 30);
         pipeline.run().waitUntilFinish();
     }
 
@@ -193,7 +280,7 @@ public class MyAggregationsTest {
         static class LogFn<E> extends DoFn<E, E> {
             @ProcessElement
             public void process(@Element E element, OutputReceiver<E> receiver, @Timestamp Instant timestamp, BoundedWindow window, PaneInfo paneInfo) {
-                LOGGER.info("Processing: {} ", String.format("%s (%s %s)", element, window.getClass().getSimpleName(), window.maxTimestamp()));
+                LOGGER.info("Processing: {} ", String.format("%s [%s]", element, windowToString(window)));
                 receiver.output(element);
             }
         }
