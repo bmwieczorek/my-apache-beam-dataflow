d066aa7415100966e782812f4346a50309487768
Bartosz Wieczorek <bmwieczorek@gmail.com>
2024-05-23 08:17:49 +0200

    added writing Json datatypes into BQ

---
+++ src/test/java/com/bawi/beam/dataflow/JsonDataTypeBQAvroWriteTest.java
@@ -0,0 +1,83 @@
+package com.bawi.beam.dataflow;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.beam.sdk.extensions.avro.coders.AvroGenericCoder;
+import org.apache.beam.sdk.io.gcp.bigquery.AvroWriteRequest;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class JsonDataTypeBQAvroWriteTest implements Serializable {
+    private static final Schema AVRO_SCHEMA = new Schema.Parser().parse(
+            """
+                    {
+                       "type" : "record",
+                       "name" : "myRecord",
+                       "fields" : [
+                        {
+                         "name" : "json_field",
+                         "type" : {
+                           "type" : "string",
+                           "sqlType" : "JSON"
+                         }
+                       }
+                      ]
+                    }
+                """);
+
+    @Rule
+    public final transient TestPipeline pipeline = TestPipeline.create();
+
+    @Test
+    public void test() throws IOException {
+            System.out.println(AVRO_SCHEMA.toString(true));
+
+            pipeline.apply(Create.of(
+                        """
+                        { "name": "John2", "age": 180 }
+                        """,
+                """
+                        { "name": "Billy2", "age": 200 }
+                        """
+            ))
+            .apply(ParDo.of(new DoFn<String, GenericRecord>() {
+                @ProcessElement
+                public void process(@Element String json, OutputReceiver<GenericRecord> receiver) {
+                    GenericRecord record = new GenericData.Record(AVRO_SCHEMA);
+                    record.put("json_field", json);
+                    receiver.output(record);
+                }
+            })).setCoder(AvroGenericCoder.of(AVRO_SCHEMA))
+            .apply(BigQueryIO.<GenericRecord>write()
+                    .withAvroFormatFunction(AvroWriteRequest::getElement)
+                    .withAvroSchemaFactory(qTableSchema -> AVRO_SCHEMA)
+                    .to("project:dataset.table")
+                    .withJsonSchema(
+                            """
+                            {
+                              "fields": [
+                                {
+                                  "name": "json_field",
+                                  "type": "JSON"
+                                }
+                              ]
+                            }
+                            """)
+                    .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+//                        .withMethod(BigQueryIO.Write.Method.FILE_LOADS)
+//                        .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
+                    .withMethod(BigQueryIO.Write.Method.STORAGE_WRITE_API)
+                    .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+        pipeline.run();
+    }
+
+}
---
+++ src/test/java/com/bawi/beam/dataflow/JsonDataTypeBQTableRowWriteTest.java
@@ -0,0 +1,76 @@
+package com.bawi.beam.dataflow;
+
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class JsonDataTypeBQTableRowWriteTest implements Serializable {
+    private static final Schema SCHEMA = createSchema();
+
+    @Rule
+    public final transient TestPipeline pipeline = TestPipeline.create();
+
+    @Test
+    public void test() throws IOException {
+            System.out.println(SCHEMA.toString(true));
+
+            pipeline.apply(Create.of(
+                        """
+                        { "name": "John", "age": 18 }
+                        """,
+                """
+                        { "name": "Billy", "age": 20 }
+                        """
+            ))
+            .apply(ParDo.of(new DoFn<String, TableRow>() {
+                @ProcessElement
+                public void process(@Element String json, OutputReceiver<TableRow> receiver) {
+//                    JsonObject jsonObject = new JsonObject();
+//                    jsonObject.addProperty("name", "John");
+//                    Gson gson = new Gson();
+//                    String json = gson.toJson(jsonObject);
+                    receiver.output(new TableRow().set("json_field", json));
+                }
+            }))
+            .apply(BigQueryIO.writeTableRows()
+                    .to("project:dataset.table")
+                    .withJsonSchema(
+                            """
+                            {
+                              "fields": [
+                                {
+                                  "name": "json_field",
+                                  "type": "JSON"
+                                }
+                              ]
+                            }
+                            """)
+                    .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+//                        .withMethod(BigQueryIO.Write.Method.FILE_LOADS)
+//                        .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
+                    .withMethod(BigQueryIO.Write.Method.STORAGE_WRITE_API)
+                    .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+        pipeline.run();
+    }
+
+    private static Schema createSchema() {
+        Schema stringSchema = Schema.create(Schema.Type.STRING);
+        stringSchema.addProp("sqlType", "JSON");
+        return SchemaBuilder.record("myRecord")
+                .fields()
+                .name("json_field").type(stringSchema).noDefault()
+                .endRecord();
+    }
+}
