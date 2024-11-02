d9c0d4786bc822d01e2540e150ed7fb19a1c38b2
Bartosz Wieczorek <bmwieczorek@gmail.com>
2024-11-02 14:04:32 +0100

    simple refactoring

--- src/main/java/com/bawi/beam/dataflow/geecon/XmlPayload.java
+++ src/main/java/com/bawi/beam/dataflow/geecon/XmlPayload.java
@@ -1,40 +1,42 @@
 package com.bawi.beam.dataflow.geecon;
 
 public class XmlPayload {
-    public static final String XML_PAYLOAD = "" +
-            "<college id=\"1\">\n" +
-            "    <staff id=\"101\" dep_name=\"Admin\">\n" +
-            "        <employee id=\"101-01\" name=\"ashish\"/>\n" +
-            "        <employee id=\"101-02\" name=\"amit\"/>\n" +
-            "        <employee id=\"101-03\" name=\"nupur\"/>\n" +
-            "        <salary id=\"101-sal\">\n" +
-            "            <basic>20000</basic>\n" +
-            "            <special-allowance>50000</special-allowance>\n" +
-            "            <medical>10000</medical>\n" +
-            "            <provident-fund>10000</provident-fund>\n" +
-            "        </salary>\n" +
-            "    </staff>\n" +
-            "    <staff id=\"102\" dep_name=\"HR\">\n" +
-            "        <employee id=\"102-01\" name=\"shikhar\"/>\n" +
-            "        <employee id=\"102-02\" name=\"sanjay\"/>\n" +
-            "        <employee id=\"102-03\" name=\"ani\"/>\n" +
-            "        <salary id=\"102-sal\">\n" +
-            "            <basic>25000</basic>\n" +
-            "            <special-allowance>60000</special-allowance>\n" +
-            "            <medical>10000</medical>\n" +
-            "            <provident-fund>12000</provident-fund>\n" +
-            "        </salary>\n" +
-            "    </staff>\n" +
-            "    <staff id=\"103\" dep_name=\"IT\">\n" +
-            "        <employee id=\"103-01\" name=\"suman\"/>\n" +
-            "        <employee id=\"103-02\" name=\"salil\"/>\n" +
-            "        <employee id=\"103-03\" name=\"amar\"/>\n" +
-            "        <salary id=\"103-sal\">\n" +
-            "            <basic>35000</basic>\n" +
-            "            <special-allowance>70000</special-allowance>\n" +
-            "            <medical>12000</medical>\n" +
-            "            <provident-fund>15000</provident-fund>\n" +
-            "        </salary>\n" +
-            "    </staff>\n" +
-            "</college>";
+    public static final String XML_PAYLOAD =
+            """
+                <college id="1">
+                    <staff id="101" dep_name="Admin">
+                        <employee id="101-01" name="ashish"/>
+                        <employee id="101-02" name="amit"/>
+                        <employee id="101-03" name="nupur"/>
+                        <salary id="101-sal">
+                            <basic>20000</basic>
+                            <special-allowance>50000</special-allowance>
+                            <medical>10000</medical>
+                            <provident-fund>10000</provident-fund>
+                        </salary>
+                    </staff>
+                    <staff id="102" dep_name="HR">
+                        <employee id="102-01" name="shikhar"/>
+                        <employee id="102-02" name="sanjay"/>
+                        <employee id="102-03" name="ani"/>
+                        <salary id="102-sal">
+                            <basic>25000</basic>
+                            <special-allowance>60000</special-allowance>
+                            <medical>10000</medical>
+                            <provident-fund>12000</provident-fund>
+                        </salary>
+                    </staff>
+                    <staff id="103" dep_name="IT">
+                        <employee id="103-01" name="suman"/>
+                        <employee id="103-02" name="salil"/>
+                        <employee id="103-03" name="amar"/>
+                        <salary id="103-sal">
+                            <basic>35000</basic>
+                            <special-allowance>70000</special-allowance>
+                            <medical>12000</medical>
+                            <provident-fund>15000</provident-fund>
+                        </salary>
+                    </staff>
+                </college>
+            """;
 }
