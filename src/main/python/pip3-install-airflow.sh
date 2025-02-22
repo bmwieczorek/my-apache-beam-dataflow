54033fc0f01fb1cdde0603e9892828dd30cce162
Bartosz Wieczorek <bmwieczorek@gmail.com>
2025-02-22 14:28:08 +0100

    added comments for pip3-install-airflow.sh

--- src/main/python/pip3-install-airflow.sh
+++ src/main/python/pip3-install-airflow.sh
@@ -3,6 +3,7 @@ AIRFLOW_VERSION=1.10.15
 PYTHON_MAJOR_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 
 echo "PYTHON MAJOR VERSION=$PYTHON_MAJOR_VERSION"
+# https://raw.githubusercontent.com/apache/airflow/constraints-1.10.15/constraints-3.8.txt
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_MAJOR_VERSION}.txt"
 echo "CONSTRAINT URL=$CONSTRAINT_URL"
 
