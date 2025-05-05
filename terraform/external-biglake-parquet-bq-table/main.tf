51d9c78711ed82e5c13bec8796756ae3d46d7297
Bartosz Wieczorek <bmwieczorek@gmail.com>
2025-05-05 20:42:00 +0200

    added working biglake with schema autodetect from empty parquet file or explicit bq schema, and with manual or automatic metadata cache refresh

--- terraform/external-biglake-parquet-bq-table/main.tf
+++ terraform/external-biglake-parquet-bq-table/main.tf
@@ -33,7 +33,7 @@ resource "google_storage_bucket_object" "empty_parquet_file" {
   bucket = google_storage_bucket.bucket.name
 }
 
-resource "null_resource" "parquet_metadata" {
+resource "null_resource" "local_parquet_metadata" {
   triggers = {
     always_run = formatdate("YYYY-MM-DD-hh-mm-ss", timestamp())
   }
@@ -50,10 +50,10 @@ resource "google_storage_bucket_iam_member" "storage_to_bq_conn_id_sa_binding" {
   member = "serviceAccount:${data.external.bq_connection_id_sa.result.serviceAccountId}"
 }
 
-resource "google_bigquery_table" "external_biglake_table" {
+resource "google_bigquery_table" "biglake_autodetect_schema_automatic_metadata_refresh" {
   project             = var.project
   dataset_id          = google_bigquery_dataset.dataset.dataset_id
-  table_id            = local.table
+  table_id            = "${local.table}_autodetect_schema_automatic_metadata_refresh"
   deletion_protection = false
   # schema = file(local.bq_schema_file)
   schema = null
@@ -63,8 +63,8 @@ resource "google_bigquery_table" "external_biglake_table" {
   external_data_configuration {
     autodetect    = true
     source_format = "PARQUET"
-    metadata_cache_mode = "MANUAL" # CALL BQ.REFRESH_EXTERNAL_METADATA_CACHE('myproject.test_db.test_table')
-    # metadata_cache_mode = "AUTOMATIC" # requires max_staleness #  googleapi: Error 400: maxStaleness must be specified when MetadataCacheMode is AUTOMATIC, invalid
+    # metadata_cache_mode = "MANUAL" # CALL BQ.REFRESH_EXTERNAL_METADATA_CACHE('myproject.test_db.test_table')
+    metadata_cache_mode = "AUTOMATIC" # requires max_staleness #  googleapi: Error 400: maxStaleness must be specified when MetadataCacheMode is AUTOMATIC, invalid
     connection_id = "${var.bq_project}.us.${data.external.bq_connection_id.result.connectionId}"
 
     parquet_options {
@@ -79,18 +79,18 @@ resource "google_bigquery_table" "external_biglake_table" {
     source_uris = ["gs://${google_storage_bucket.bucket.name}/${local.table}/*.parquet"]
   }
 
-  depends_on = [google_storage_bucket_object.empty_parquet_file, null_resource.parquet_metadata]
+  depends_on = [google_storage_bucket_object.empty_parquet_file, null_resource.local_parquet_metadata]
 }
 
-resource "null_resource" "bq_show_schema" {
+resource "null_resource" "bq_show_autodetected_table_schema" {
   triggers = {
     always_run = formatdate("YYYY-MM-DD-hh-mm-ss", timestamp())
   }
 
   provisioner "local-exec" {
-    command = "bq show --format=prettyjson ${google_bigquery_table.external_biglake_table.project}:${google_bigquery_table.external_biglake_table.dataset_id}.${google_bigquery_table.external_biglake_table.table_id} | jq '.schema.fields' | jq '.[] | select(.name==\"myRequiredDateTime\")'"
+    command = "bq show --format=prettyjson ${google_bigquery_dataset.dataset.project}:${google_bigquery_dataset.dataset.dataset_id}.${google_bigquery_table.biglake_autodetect_schema_automatic_metadata_refresh.table_id} | jq '.schema.fields' | jq '.[] | select(.name==\"myRequiredDateTime\")'"
   }
-  depends_on = [google_bigquery_table.external_biglake_table]
+  depends_on = [google_bigquery_table.biglake_autodetect_schema_automatic_metadata_refresh]
 }
 
 resource "null_resource" "bq_select_external_biglake_table_with_empty_file_only" {
@@ -99,10 +99,10 @@ resource "null_resource" "bq_select_external_biglake_table_with_empty_file_only"
   }
 
   provisioner "local-exec" {
-    command = "bq query --use_legacy_sql=false 'SELECT count(*) AS cnt FROM ${google_bigquery_table.external_biglake_table.dataset_id}.${google_bigquery_table.external_biglake_table.table_id} WHERE year is not null'"
+    command = "bq query --use_legacy_sql=false 'SELECT count(*) AS cnt FROM ${google_bigquery_dataset.dataset.project}.${google_bigquery_dataset.dataset.dataset_id}.${google_bigquery_table.biglake_autodetect_schema_automatic_metadata_refresh.table_id} WHERE year is not null'"
   }
   depends_on = [
-    null_resource.bq_show_schema,
+    null_resource.bq_show_autodetected_table_schema,
     google_storage_bucket_iam_member.storage_to_bq_conn_id_sa_binding # Access Denied: bqcx-xxxx-yyy@gcp-sa-bigquery-condel.iam.gserviceaccount.com does not have storage.objects.list access to the Google Cloud Storage bucket. Permission 'storage.objects.list'. Please make sure gs://bucket/external_biglake_table/*.parquet is accessible via appropriate IAM roles, e.g. Storage Object Viewer or Storage Object Creator.
   ]
 }
@@ -120,9 +120,63 @@ resource "null_resource" "bq_select_external_biglake_table_with_non_empty_file" 
   }
 
   provisioner "local-exec" {
-    command = "bq query --use_legacy_sql=false 'SELECT * FROM ${google_bigquery_table.external_biglake_table.dataset_id}.${google_bigquery_table.external_biglake_table.table_id} WHERE year is not null'"
+    command = "bq query --use_legacy_sql=false 'SELECT * FROM ${google_bigquery_dataset.dataset.project}.${google_bigquery_dataset.dataset.dataset_id}.${google_bigquery_table.biglake_autodetect_schema_automatic_metadata_refresh.table_id} WHERE year is not null'"
   }
 
   depends_on = [null_resource.bq_select_external_biglake_table_with_empty_file_only, google_storage_bucket_object.non_empty_parquet_file]
 }
 
+resource "google_bigquery_table" "biglake_explicit_schema_manual_metadata_refresh" {
+  project             = var.project
+  dataset_id          = google_bigquery_dataset.dataset.dataset_id
+  table_id            = "${local.table}_explicit_schema_manual_metadata_refresh"
+  deletion_protection = false
+  schema = file(local.bq_schema_file)
+  # schema = null
+  max_staleness = "0-0 7 0:0:0" # 7 days
+
+
+  external_data_configuration {
+    autodetect    = false
+    source_format = "PARQUET"
+    metadata_cache_mode = "MANUAL" # CALL BQ.REFRESH_EXTERNAL_METADATA_CACHE('myproject.test_db.test_table')
+    # metadata_cache_mode = "AUTOMATIC" # requires max_staleness #  googleapi: Error 400: maxStaleness must be specified when MetadataCacheMode is AUTOMATIC, invalid
+    connection_id = "${var.bq_project}.us.${data.external.bq_connection_id.result.connectionId}"
+
+    parquet_options {
+      enable_list_inference = true
+    }
+
+    hive_partitioning_options {
+      mode              = "CUSTOM"
+      source_uri_prefix = "gs://${google_storage_bucket.bucket.name}/${local.table}/{year:STRING}/{month:STRING}/{day:STRING}/{hour:STRING}"
+    }
+
+    source_uris = ["gs://${google_storage_bucket.bucket.name}/${local.table}/*.parquet"]
+  }
+
+  depends_on = [google_storage_bucket_object.empty_parquet_file, null_resource.local_parquet_metadata]
+}
+
+resource "null_resource" "refresh_metadata_cache" {
+  triggers = {
+    always_run = formatdate("YYYY-MM-DD-hh-mm-ss", timestamp())
+  }
+
+  provisioner "local-exec" {
+    command = "bq query --use_legacy_sql=false 'CALL BQ.REFRESH_EXTERNAL_METADATA_CACHE(\"${google_bigquery_dataset.dataset.project}.${google_bigquery_dataset.dataset.dataset_id}.${google_bigquery_table.biglake_explicit_schema_manual_metadata_refresh.table_id}\")'"
+  }
+
+  depends_on = [google_bigquery_table.biglake_explicit_schema_manual_metadata_refresh]
+}
+
+resource "null_resource" "bq_show_explicit_table_schema" {
+  triggers = {
+    always_run = formatdate("YYYY-MM-DD-hh-mm-ss", timestamp())
+  }
+
+  provisioner "local-exec" {
+    command = "bq show --format=prettyjson ${google_bigquery_dataset.dataset.project}:${google_bigquery_dataset.dataset.dataset_id}.${google_bigquery_table.biglake_explicit_schema_manual_metadata_refresh.table_id} | jq '.schema.fields' | jq '.[] | select(.name==\"myRequiredDateTime\")'"
+  }
+  depends_on = [google_bigquery_table.biglake_explicit_schema_manual_metadata_refresh]
+}
