1053c43723ef5afd7f7f36d5d4c13a735c21f452
Bartosz Wieczorek <bmwieczorek@gmail.com>
2025-05-11 20:54:26 +0200

    working example of writing to bq as json field type

--- src/main/python/beam/my_simple_json_to_bq_job.py
+++ src/main/python/beam/my_simple_json_to_bq_job.py
@@ -18,6 +18,11 @@ class MyDoFn(beam.DoFn):
         parsed_data = json.loads(element)
         logging.info("INFO loading JSON data from %s to %s", element, type(parsed_data))
         yield parsed_data
+#        logging.info("INFO loading JSON data from %s to %s", parsed_data['user_id'], parsed_data['event_properties'])
+#        yield {
+#            "user_id": parsed_data['user_id'],
+#            "event_properties": json.dumps(parsed_data['event_properties'])
+#        }
 
 
 def run():
@@ -32,6 +37,12 @@ def run():
             {"name": "event_properties", "type": "json"}
         ]
     }"""
+#    bq_json_schema_string = """{
+#        "fields": [
+#            {"name": "user_id", "type": "STRING"},
+#            {"name": "event_properties", "type": "STRING"}
+#        ]
+#    }"""
     bq_table_schema = parse_table_schema_from_json(bq_json_schema_string)
 
     # The pipeline will be run on exiting the with block.
@@ -53,6 +64,7 @@ def run():
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
             write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
             temp_file_format=FileFormat.JSON
+#            temp_file_format=FileFormat.AVRO
         )
 
 
@@ -63,7 +75,7 @@ if __name__ == '__main__':
 # direct runner
 # python3 src/main/python/beam/my_simple_json_to_bq_job.py \
 #  --temp_location "gs://${GCP_PROJECT}-${GCP_OWNER}/python-tmp" \
-#  --output_table "${GCP_PROJECT}.bartek_person.my_json_table4"
+#  --output_table "${GCP_PROJECT}.${GCP_OWNER}_dataset.my_json_table"
 
 # dataflow runner
 # python3 src/main/python/beam/my_simple_json_to_bq_job.py \
@@ -75,4 +87,54 @@ if __name__ == '__main__':
 #  --no_use_public_ips \
 #  --subnetwork ${GCP_SUBNETWORK} \
 #  --temp_location gs://${GCP_PROJECT}-${GCP_OWNER}/python-tmp \
-#  --output_table ${GCP_PROJECT}.bartek_person.my_json_table4
+#  --output_table ${GCP_PROJECT}.${GCP_OWNER}_dataset.my_json_table
+
+
+# bq schema json type
+#bq show --format=prettyjson --schema ${GCP_OWNER}_dataset.my_json_table
+#[
+#    {
+#        "mode": "NULLABLE",
+#        "name": "user_id",
+#        "type": "STRING"
+#    },
+#    {
+#        "mode": "NULLABLE",
+#        "name": "event_properties",
+#        "type": "JSON"
+#    }
+#]
+
+# bq query --nouse_legacy_sql 'SELECT * FROM ${GCP_OWNER}_dataset.my_json_table
+# +---------+------------------------------------------+
+# | user_id |             event_properties             |
+# +---------+------------------------------------------+
+# | id1     | {"myIntProp":1000,"myStrProp1":"myVal1"} |
+# | id2     | {"myIntProp":2000,"myStrProp2":"myVal2"} |
+# +---------+------------------------------------------+
+
+
+# bq schema STRING type
+#bq show --format=prettyjson --schema ${GCP_OWNER}_dataset.my_json_table
+#[
+#    {
+#        "mode": "NULLABLE",
+#        "name": "user_id",
+#        "type": "STRING"
+#    },
+#    {
+#        "mode": "NULLABLE",
+#        "name": "event_properties",
+#        "type": "STRING"
+#    }
+#]
+
+#bq query --nouse_legacy_sql 'SELECT * FROM ${GCP_OWNER}_dataset.my_json_table'
+#+---------+---------------------------------------------+
+#| user_id |              event_properties               |
+#+---------+---------------------------------------------+
+#| id1     | {"myStrProp1": "myVal1", "myIntProp": 1000} |
+#| id2     | {"myStrProp2": "myVal2", "myIntProp": 2000} |
+#+---------+---------------------------------------------+
+
+# json schema type and temp_file_format=FileFormat.AVRO gives error: AssertionError: Unable to map BigQuery field type json to avro type [while running 'WriteToBigQuery/BigQueryBatchFileLoads/ParDo(WriteRecordsToFile)/ParDo(WriteRecordsToFile)']
