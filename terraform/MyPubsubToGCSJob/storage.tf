c1b095e7b93445db581987152aa348caa3dd1a1d
Bartosz Wieczorek <bmwieczorek@gmail.com>
2026-03-21 08:12:09 +0100

    fix: resolve permission issues for BigQuery and Pub/Sub resources in Terraform configuration for draining dataflow job

--- terraform/MyPubsubToGCSJob/storage.tf
+++ terraform/MyPubsubToGCSJob/storage.tf
@@ -1,8 +1,16 @@
 resource "google_storage_bucket" "my_bucket" {
   name          = local.bucket
-  project	    = var.project
+  project       = var.project
   location      = var.region
   storage_class = "REGIONAL"
   force_destroy = true
   labels        = local.labels
 }
+
+# to be enabled if storage errors on dataflow logs upon draining
+# resource "google_storage_bucket_iam_member" "grant_sa_object_admin_role_to_bucket" {
+#   project = var.project
+#   bucket  = google_storage_bucket.my_bucket.name
+#   role    = "roles/storage.objectAdmin"
+#   member  = "serviceAccount:${var.service_account}"
+# }
