331ce83067eaf4a60c9331cc98c3efcb575b924f
Bartosz Wieczorek <bmwieczorek@gmail.com>
2026-03-27 09:08:39 +0100

    refactor GZipDecompressionOnTheFlyTest and GzipDecompressionOnTheFlyJobTest to use GzipUtils for compression and decompression, and rename readFwdAsBytes to readXmlChunkAsBytes for clarity

--- src/test/java/com/bawi/beam/dataflow/GzipDecompressionOnTheFlyJobTest.java
+++ src/test/java/com/bawi/beam/dataflow/GzipDecompressionOnTheFlyJobTest.java
@@ -1,19 +1,23 @@
 package com.bawi.beam.dataflow;
 
+import com.bawi.io.SplitInputStream;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
+
+import static com.bawi.io.GzipUtils.gunzip;
+import static com.bawi.io.GzipUtils.gzip;
 
 public class GzipDecompressionOnTheFlyJobTest {
     @Rule
@@ -72,88 +76,6 @@ public class GzipDecompressionOnTheFlyJobTest {
     }
 
 
-    private byte[] gzip(String s) throws IOException {
-        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(bytes.length);
-        try (GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteStream)) {
-            gzipOutputStream.write(bytes);
-        }
-        return byteStream.toByteArray();
-    }
 
-    static byte[] gunzip(byte[] compressedPayload) throws IOException {
-        try (
-                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(compressedPayload);
-                GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream)
-        ) {
-            return IOUtils.toByteArray(gzipInputStream);
-        }
-    }
 
-    private static class SplitInputStream implements AutoCloseable {
-
-        byte[] startTag;
-
-        byte[] endTag;
-
-        private final InputStream is;
-
-        private static final int MAX_BYTES_READ = 1024 * 1024;
-
-        public SplitInputStream(InputStream is, String startTag, String endTag) {
-            this.is = is;
-            this.startTag = startTag.getBytes(StandardCharsets.UTF_8);
-            this.endTag = endTag.getBytes(StandardCharsets.UTF_8);
-        }
-
-        public byte[] readXmlChunkAsBytes() throws IOException {
-            ByteArrayOutputStream out = new ByteArrayOutputStream(1024 * 128);
-            if (readUntilMatch(startTag, out, true)) {
-                out.write(startTag);
-                if (readUntilMatch(endTag, out, false)) {
-                    return out.toByteArray();
-                }
-            }
-            return null;
-        }
-
-        private boolean readUntilMatch(byte[] needle, ByteArrayOutputStream dataRead, boolean skipData) throws IOException {
-            int i = 0;
-            int pos = -1;
-            while (true) {
-                pos++;
-                int b = is.read();
-                // end of file:
-                if (b == -1) {
-                    return false;
-                }
-
-                // skip or save to buffer:
-                if (!skipData) {
-                    dataRead.write(b);
-                }
-
-                // check if we're matching:
-                if (b == needle[i]) {
-                    i++;
-                    if (i >= needle.length) {
-                        return true;
-                    }
-                } else {
-                    i = 0;
-                }
-
-                // TODO: verify if condition: pos >= MAX_BYTES_READ is necessary
-                // see if we've passed the stop point:
-                if (skipData && i == 0 && pos >= MAX_BYTES_READ) {
-                    return false;
-                }
-            }
-        }
-
-        @Override
-        public void close() throws Exception {
-            is.close();
-        }
-    }
 }
