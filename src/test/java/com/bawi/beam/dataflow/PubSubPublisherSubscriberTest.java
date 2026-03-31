8ce6b2de1bdc9e7823417976ff53b349a58cc4d7
Bartosz Wieczorek <bmwieczorek@gmail.com>
2026-03-31 09:54:48 +0200

    refactor PubSubPublisherSubscriberTest to create autocloseable MySubscriber and added polling mechanism

--- src/test/java/com/bawi/beam/dataflow/PubSubPublisherSubscriberTest.java
+++ src/test/java/com/bawi/beam/dataflow/PubSubPublisherSubscriberTest.java
@@ -9,7 +9,6 @@ import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
-import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,101 +16,111 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Consumer;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 import static com.bawi.beam.dataflow.PubSubUtils.createTopicAndSubscription;
 import static com.bawi.beam.dataflow.PubSubUtils.deleteSubscriptionAndTopic;
+import static com.bawi.io.GzipUtils.gunzip;
+import static com.bawi.io.GzipUtils.gzip;
 
+@SuppressWarnings("SameParameterValue")
 public class PubSubPublisherSubscriberTest {
-    public static final Logger LOGGER = LoggerFactory.getLogger(PubSubPublisherSubscriberTest.class);
 
     @Test
-    public void test() throws IOException, ExecutionException, InterruptedException {
+    public void shouldPublishAndReceiveGzipCompressedMessages() throws Exception {
         // given
         String text1 = "abc123";
         String text2 = "cdef4567";
 
         // when
-        publish(PROJECT, TOPIC_NAME, compress(text1.getBytes()));
-        publish(PROJECT, TOPIC_NAME, compress(text2.getBytes()));
-        List<byte[]> messages = subscribe(PROJECT, SUBSCRIPTION_NAME);
+        publish(PROJECT, TOPIC_NAME, gzip(text1));
+        publish(PROJECT, TOPIC_NAME, gzip(text2));
+
+        List<byte[]> receivedMessages;
+        try (MySubscriber mySubscriber = new MySubscriber(PROJECT, SUBSCRIPTION_NAME)) {
+            receivedMessages = pollExpectedNMessagesWithRetry(mySubscriber, 2, 10);
+        }
+        List<String> receivedDecompressedMessage = receivedMessages.stream().map(bytes -> new String(gunzip(bytes))).toList();
 
         // then
-        Assert.assertEquals(2, messages.size());
-        List<String> receivedDecompressedMessage = messages.stream().map(bytes -> new String(decompress(bytes))).toList();
+        Assert.assertEquals(2, receivedMessages.size());
         Assert.assertTrue(receivedDecompressedMessage.contains(text1));
         Assert.assertTrue(receivedDecompressedMessage.contains(text2));
     }
 
-    private static List<byte[]> subscribe(String project, String subscription) {
+    private static List<byte[]> pollExpectedNMessagesWithRetry(MySubscriber mySubscriber, int expectedCount, int maxRetries) throws InterruptedException {
         List<byte[]> messages = new ArrayList<>();
-        try {
-            subscribe(project, subscription, messages::add);
-            return messages;
-        } catch (TimeoutException e) {
-            return messages;
+        for (int retryCounter = 0; retryCounter < maxRetries; retryCounter++) {
+            messages = mySubscriber.getMessages();
+            if (messages.size() < expectedCount) {
+                LOGGER.info("Waiting for messages to be received...");
+                TimeUnit.SECONDS.sleep(1);
+            } else {
+                break;
+            }
         }
+        return messages;
     }
 
-    private static void subscribe(String project, String subscription, Consumer<byte[]> c) throws TimeoutException {
-        MessageReceiver receiver = (PubsubMessage message, AckReplyConsumer consumer) -> {
-            ByteString data = message.getData();
-            byte[] bytes = data.toByteArray();
-            c.accept(bytes);
-            LOGGER.info("Received: message.getMessageId()={}, message.getData().size()={}", message.getMessageId(), message.getData().size());
-            consumer.ack();
-        };
+    public static final Logger LOGGER = LoggerFactory.getLogger(PubSubPublisherSubscriberTest.class);
 
-        ProjectSubscriptionName projectSubscription = ProjectSubscriptionName.of(project, subscription);
-        Subscriber subscriber = Subscriber.newBuilder(projectSubscription, receiver).build();
-        LOGGER.info("Starting subscribing to project: {}, subscription: {}", projectSubscription, subscription);
-        subscriber.startAsync().awaitRunning();
-        subscriber.awaitTerminated(10, TimeUnit.SECONDS);
-        LOGGER.info("Stopped subscribing to project: {}, subscription: {}", projectSubscription, subscription);
+    static class MySubscriber implements AutoCloseable {
+        private final List<byte[]> messages = new CopyOnWriteArrayList<>();
 
+        private final String project;
+        private final String subscription;
+        private final Subscriber subscriber;
+
+        public MySubscriber(String project, String subscription) {
+            this.project = project;
+            this.subscription = subscription;
+            MessageReceiver receiver = (PubsubMessage message, AckReplyConsumer ackReplyConsumer) -> {
+                ByteString data = message.getData();
+                byte[] bytes = data.toByteArray();
+                messages.add(bytes);
+                LOGGER.info("Received: message.getMessageId()={}, message.getData().size()={}", message.getMessageId(), message.getData().size());
+                ackReplyConsumer.ack();
+            };
+
+            ProjectSubscriptionName projectSubscription = ProjectSubscriptionName.of(project, subscription);
+            subscriber = Subscriber.newBuilder(projectSubscription, receiver).build();
+            LOGGER.info("Starting subscribing to project: {}, subscription: {}", project, subscription);
+            subscriber.startAsync().awaitRunning();
+        }
+
+        @Override
+        public void close() throws TimeoutException {
+            LOGGER.info("Stopping subscribing to project: {}, subscription: {}", project, subscription);
+            subscriber.stopAsync().awaitTerminated(10, TimeUnit.SECONDS);
+            LOGGER.info("Stopped subscribing to project: {}, subscription: {}", project, subscription);
+        }
+
+        public List<byte[]> getMessages() {
+            return new ArrayList<>(messages);
+        }
     }
 
     private static void publish(String project, String topic, byte[] bytes) throws IOException, InterruptedException, ExecutionException {
         Publisher publisher = Publisher.newBuilder(TopicName.of(project, topic)).build();
-        ByteString byteString = ByteString.copyFrom(bytes);
-        LOGGER.info("Publishing: data with byteString size={}", byteString.size());
-        PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(byteString).build();
-        ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
-        String messageId = messageIdFuture.get();
-        publisher.shutdown();
-        publisher.awaitTermination(20, TimeUnit.SECONDS);
-        LOGGER.info("Published: messageId={}", messageId);
-    }
-
-    private static byte[] compress(byte[] bytes) throws IOException {
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(bytes.length);
-        try (GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteStream)) {
-            gzipOutputStream.write(bytes);
-        }
-        return byteStream.toByteArray();
-    }
-
-    static byte[] decompress(byte[] compressedPayload) {
-        try (
-                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(compressedPayload);
-                GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream)
-        ) {
-            return IOUtils.toByteArray(gzipInputStream);
-        } catch (IOException e) {
-            LOGGER.error("Error decompressing payload", e);
-            return new byte[0];
+        try {
+            ByteString byteString = ByteString.copyFrom(bytes);
+            LOGGER.info("Publishing: data with byteString size={}", byteString.size());
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(byteString).build();
+            ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+            String messageId = messageIdFuture.get();
+            LOGGER.info("Published: messageId={}", messageId);
+        } finally {
+            publisher.shutdown();
+            publisher.awaitTermination(20, TimeUnit.SECONDS);
         }
     }
+
 
     private static final String PROJECT = System.getenv("GCP_PROJECT");
     private static final String TOPIC_NAME = System.getenv("GCP_OWNER") + "-" + "topic";
